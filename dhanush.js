93d8f7a06526af15dd73a79ce7bd2e37326b6f0c
Gowri Shankar <shankarg.kvsulur@gmail.com>
2019-09-08 22:04:50 +0530

    asd

---
+++ dhanush.js
@@ -0,0 +1,37 @@
+//var a = 5;
+var b = 10;
+var c = 20;
+console.log("a+b=");
+result = a + b;
+console.log("result");
+console.log("result", result);
+var a = 15;
+var b = 25;
+var c = 24;
+console.log("a=", a);
+console.log("b=", b);
+console.log("c=", c);
+result1 = a + b;
+console.log("a+b", result1);
+result2 = a + c;
+console.log("a+c", result2);
+result3 = b + c;
+console.log("b+c", result3);
+var day = "wednesday";
+switch (day) {
+  case "sunday":
+    console.log("this is sunday");
+    break;
+  case "monday":
+    console.log("monday");
+    break;
+  case "tuesday":
+    console.log("tuesday");
+    break;
+  case "wednesday":
+    console.log("wednesday");
+    break;
+  case "thuresday":
+    console.log("thuresday");
+    break;
+}
